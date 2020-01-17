674f1a20eb20ef61cc889ae4b481b643e1d5b0c1
mariselagonzalez <37843508+mariselagonzalez@users.noreply.github.com>
2020-01-17 15:00:03 -0800

    Add files via upload

---
+++ stranger.rb
@@ -0,0 +1,20 @@
+use_bpm 145
+live_loop :one do
+  play :c4
+  sleep 0.4
+  play :e4
+  sleep 0.4
+  play :g4
+  sleep 0.4
+  play :b4
+  sleep 0.4
+  play :c5
+  sleep 0.4
+  play :b4
+  sleep 0.4
+  play :g4
+  sleep 0.4
+  play :e4
+  sleep 0.4
+end
+
